df1fb575e1049100645dc32e95c3c47ab6ed8f7e
zhengchangdi_mac_vm <mac_vm@home.com>
2017-12-08 21:03:58 +0800

    爬 爱每刻 这个网站。。。。出乎意料的简单。。。。。。。

---
+++ aimeike/spider.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+url = 'https://aimeike.tv/category'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+source_list = soup.find('div', class_="category-gallery").find_all('a', class_="category-item theme-item card js-category-item")
+target = []
+for source in source_list:
+    data = {
+        'title': source.find('h5').get_text(),
+        'tag': source.find('time').get_text(),
+        'user_counet': source.find('span').get_text(),
+        'text': source.find('p').get_text(),
+    }
+    target.append(data)
+
